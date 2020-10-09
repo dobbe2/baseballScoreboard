400eb69dfe823e2220bc00a88e1574a3e2812ece
James Dobbe <dobbe2@gmail.com>
2020-10-08 23:49:02 -0700

    added functionaily for the balls, strikes, and outs

---
+++ script.js
@@ -0,0 +1,88 @@
+let homeTeam = "";
+let awwayTeam = "";
+let outs = 0;
+let balls = 0;
+let strikes = 0;
+
+$(document).ready(function(){
+
+    $("#add-ball").click(function(){
+        if($('#ball-one').hasClass('counter-dot-empty')) {
+            console.log("Ball 1")
+            $("#ball-one").removeClass('counter-dot-empty').addClass("counter-dot")
+        }else if($('#ball-two').hasClass('counter-dot-empty')){
+            console.log("Ball 2")
+            $("#ball-two").removeClass('counter-dot-empty').addClass("counter-dot");
+        }else if($('#ball-three').hasClass('counter-dot-empty')){
+            console.log("Ball 3")
+            $("#ball-three").removeClass('counter-dot-empty').addClass("counter-dot");
+        }else if($('#ball-four').hasClass('counter-dot-empty')){
+            console.log("Ball 4")
+            $("#ball-four").removeClass('counter-dot-empty').addClass("counter-dot");
+        }
+    })
+
+    $("#add-strike").click(function(){
+        if($('#strike-one').hasClass('counter-dot-empty')) {
+            console.log("Strike 1")
+            $("#strike-one").removeClass('counter-dot-empty').addClass("counter-dot")
+        }else if($('#strike-two').hasClass('counter-dot-empty')){
+            console.log("Strike 2")
+            $("#strike-two").removeClass('counter-dot-empty').addClass("counter-dot");
+        }else if($('#strike-three').hasClass('counter-dot-empty')){
+            console.log("Strike 3")
+            $("#strike-three").removeClass('counter-dot-empty').addClass("counter-dot");
+            setTimeout(function(){
+                $("#strike-one").addClass("counter-dot-empty").removeClass('counter-dot');
+                $("#strike-two").addClass("counter-dot-empty").removeClass('counter-dot');
+                $("#strike-three").addClass("counter-dot-empty").removeClass('counter-dot');
+
+                if($('#out-one').hasClass('counter-dot-empty')) {
+                    console.log("Out 1")
+                    $("#out-one").removeClass('counter-dot-empty')
+                    $("#out-one").addClass("counter-dot")
+                }else if($('#out-two').hasClass('counter-dot-empty')){
+                    console.log("Out 2")
+                    $("#out-two").removeClass('counter-dot-empty');
+                    $("#out-two").addClass("counter-dot");
+                }else if($('#out-three').hasClass('counter-dot-empty')){
+                    console.log("Out 3")
+                    $("#out-three").removeClass('counter-dot-empty');
+                    $("#out-three").addClass("counter-dot");
+                }
+            },1000)
+        }
+    })
+
+    $("#add-out").click(function(){
+        if($('#out-one').hasClass('counter-dot-empty')) {
+            console.log("Out 1")
+            $("#out-one").removeClass('counter-dot-empty').addClass("counter-dot")
+        }else if($('#out-two').hasClass('counter-dot-empty')){
+            console.log("Out 2")
+            $("#out-two").removeClass('counter-dot-empty').addClass("counter-dot");
+        }else if($('#out-three').hasClass('counter-dot-empty')){
+            console.log("Out 3")
+            $("#out-three").removeClass('counter-dot-empty').addClass("counter-dot");
+        }
+    })
+    
+})
+
+//still figuring out how to parse this JSON return
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api-baseball.p.rapidapi.com/teams?league=1&season=2020",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-baseball.p.rapidapi.com",
+		"x-rapidapi-key": "eef4bb6860mshbcdd99ffb0c7afbp140049jsn172da8baa055"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    let diamondbacks = response[0].id;
+    console.log(diamondbacks)
+});
